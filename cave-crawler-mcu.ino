c4b0732078f1b21bee45c5c8a166135f0617f2a7
Bartosz Meglicki <bmegli@op.pl>
2018-08-19 12:17:34 +0200

    asynchronous EM7180 I/O
    Reduces the max main loop time from 650 microseconds to 20 microseconds.
    (when reading quaternion data from EM7180).
    
    The change is necessary for precise timestamping of other sensors data.
    EM7180 and encoders will get right timestamps anyway due to interrupt timestamping.
    Other sensors will rely on loop time unfortunetly so EM7180 can't block for half ms.
    
    This is much more complicated unfortunately:
    - state machine in EM7180 library (to handle async TX and RX)
    - state machine in cave-crawler-mcu (to handle new data, status and quaternion)

--- cave-crawler-mcu.ino
+++ cave-crawler-mcu.ino
@@ -60,6 +60,9 @@ const uint16_t GYRO_RATE      = 200;  // Hz
 const uint8_t  BARO_RATE      = 50;   // Hz
 const uint8_t  Q_RATE_DIVISOR = 2;    // 1/2 gyro rate
 
+enum ImuState {WAITING_FOR_NEW_DATA, READING_STATUS, READING_QUATERNION};
+ImuState imuState;
+
 /* Data types */
 
 struct imu_packet
@@ -107,6 +110,11 @@ unsigned long last_print_time_us;
 unsigned long max_loop_time_us;
 //end temp
 
+//temp
+#define MEM_LEN 256
+char databuf[MEM_LEN];
+//endtemp
+
 void setup()
 {
   Serial.begin(115200);
@@ -124,9 +132,9 @@ void setup()
 void loop()
 {
   processIMU();
-  processLidar();
+  //processLidar();
   
-  vertical_lidar.applyMotorPID();
+  //vertical_lidar.applyMotorPID();
 
   timeStats();
 }
@@ -146,57 +154,90 @@ void processLidar()
   packet.speed64 = xv11_packet.speed64;
   memcpy(packet.distances, xv11_packet.distances, sizeof(packet.distances));
   
-  //encode & empit packet over USB
+  //encode & emit packet over USB
 }
 
 void imuInterruptHandler()
 {
-    imu_timestamp_us=micros();
-    imu_new_data = true;    
+  imu_timestamp_us=micros();
+  imu_new_data = true;    
 }
 
-unsigned long max_diff;
+unsigned long max_diff; //temp
 
 void processIMU()
 {
-  if(!imu_new_data)
-    return;
-  
-  imu_new_data = false;
-
   imu_packet packet;
+  unsigned long diff;
 
-  noInterrupts();
-    packet.timestamp_us=imu_timestamp_us;
-  interrupts();
+  switch(imuState)
+  {
+  case WAITING_FOR_NEW_DATA:
+    if(!imu_new_data)
+      return;
+
+    imu_new_data = false;
   
-  unsigned long diff=micros() - packet.timestamp_us;
-  if(diff > max_diff)
-    max_diff=diff;
+    noInterrupts();
+      packet.timestamp_us=imu_timestamp_us;
+    interrupts();
 
-  packet.left_encoder_counts=left_encoder.read();
-  packet.right_encoder_counts=right_encoder.read();
+    diff=micros() - packet.timestamp_us;
+    if(diff > max_diff)
+      max_diff=diff;
 
-  em7180.checkEventStatus(); // also clears the interrupt
+    packet.left_encoder_counts=left_encoder.read();
+    packet.right_encoder_counts=right_encoder.read();
 
-  if (em7180.gotError())
-  {
-    Serial.print("ERROR: ");
-    Serial.println(em7180.getErrorString());
-    return;
-  }
+    imuState=READING_STATUS;
+    
+  case READING_STATUS:
+    if(!em7180.checkEventStatusAsync())
+      return;    
 
-  if (em7180.gotQuaternion())
-  {
-    em7180.readQuaternion(packet.qw, packet.qx, packet.qy, packet.qz);  
+    if(Wire.getError())
+    {
+      Serial.print("Wire ERROR: ");
+      Serial.println(Wire.getError(), DEC);
+    }
+    if (em7180.gotError() || Wire.getError()) 
+    {
+      Serial.print("em7180 ERROR: ");
+      Serial.print(em7180.getErrorString());
+      Serial.print("Wire status ");
+      Serial.println(Wire.status(), DEC);
+      imuState=WAITING_FOR_NEW_DATA;
+      return;
+    }
+
+    if (em7180.gotQuaternion())
+      imuState=READING_QUATERNION;
+    
+  case READING_QUATERNION:
+    if(!em7180.readQuaternionAsync(packet.qw, packet.qx, packet.qy, packet.qz))
+      return;
+
+    if (Wire.getError()) 
+    {
+      Serial.print("Wire status ");
+      Serial.println(Wire.status(), DEC);
+      imuState=WAITING_FOR_NEW_DATA;
+      return;
+    }
+
+    imuState=WAITING_FOR_NEW_DATA;
     //encode & emit packet over USB
+
+    Serial.println("got quaternion");
   }
+  
 }
 
 
 void setupIMU()
 {
   Wire.begin(I2C_MASTER, 0x00, I2C_PINS_18_19, I2C_PULLUP_EXT, I2C_RATE_400);
+  
   pinMode(IMU_INTERRUPT_PIN, INPUT);
   attachInterrupt(IMU_INTERRUPT_PIN, imuInterruptHandler, RISING);  
 
